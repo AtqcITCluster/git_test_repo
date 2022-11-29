33a3232d7e218621b02364dc6e476b05bd871f02
Artem Korchevyi <artem.korchevy@gmail.com>
2022-11-29 14:11:34 +0200

    added new class, update exist

--- src/test/java/atqc/ClassOneGit.java
+++ src/test/java/atqc/ClassOneGit.java
@@ -2,6 +2,11 @@ package atqc;
 
 public class ClassOneGit {
     public static void main(String[] args) {
+        printMessage();
+    }
+
+    public static void printMessage(){
         System.out.println("class one run");
     }
+
 }
